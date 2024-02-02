082b41dc164ef63779b8461609373817a060cf87
balabalajb <1213911124@qq.com>
2024-02-02 10:48:45 +0800

    version: 1.0.27

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     }, //库编译模式配置
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'element-plus/dist/index.css'],
+      external: ['vue', 'element-plus'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
